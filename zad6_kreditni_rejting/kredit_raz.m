9afa4198f64229ad56a77329404c9c7b2fa938e0
HannibalVector <ikotarac@student.math.hr>
2015-03-19 12:53:15 +0100

    Dovrseno na 3. predavanju

---
+++ zad6_kreditni_rejting/kredit_raz.m
@@ -0,0 +1,26 @@
+function [ ] = kredit_raz( n )
+%KREDIT_RAZ Summary of this function goes here
+%   Detailed explanation goes here
+
+load('model_kredit_A.mat');
+u = zeros(8, 1);
+u(1) = 1;
+
+p = plot(u, 'b-o');
+set(gca,'XTick', 1:8);
+set(gca,'XTickLabel', { 'AAA', 'AA', 'A', 'BBB', 'BB', 'B', 'CCC', 'D' } );
+xlabel('stanje');
+ylabel('gustoca');
+axis([0.5 8.5 0 1]);
+
+for i=1:(n+1)
+    title(sprintf('iteracija br. %d', i));
+    u = A*u;
+    pause(0.1);
+    set(p, 'Ydata', u);
+    refreshdata
+    drawnow
+end
+
+end
+
